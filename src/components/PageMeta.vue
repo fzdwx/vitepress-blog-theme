03c6ba9c6d89a6e2a100089cd54c759a1f8c5035
fzdwx <likelovec@gmail.com>
2023-03-25 15:26:11 +0800

    update tag style

--- src/components/PageMeta.vue
+++ src/components/PageMeta.vue
@@ -27,34 +27,44 @@ const editLink = () => {
 </script>
 
 <template>
-  <div class="pageMeta flex items-center dark:text-dark-text/[.86]">
-    <svg class="w-4 h-4 mr-1" fill="currentColor" viewBox="0 0 20 20">
-      <path
-        d="M10 18a8 8 0 100-16 8 8 0 000 16zm0-2a6 6 0 110-12 6 6 0 010 12zM9 9a1 1 0 011-1h1a1 1 0 110 2H10a1 1 0 01-1-1zM9 11a1 1 0 00-1 1v3a1 1 0 102 0v-3a1 1 0 00-1-1zM9 5a1 1 0 100 2 1 1 0 000-2z"
-      ></path>
-    </svg>
-    <span class="text-sm mr-4">{{ formatDate(page.date) }}</span>
-
-    <svg class="w-4 h-4 mr-1" fill="currentColor" viewBox="0 0 20 20">
-      <path
-        d="M10 18a8 8 0 100-16 8 8 0 000 16zm0-2a6 6 0 110-12 6 6 0 010 12zM5 11a1 1 0 011-1h6a1 1 0 110 2H6a1 1 0 01-1-1zM14 8a1 1 0 00-1-1h-3a1 1 0 100 2h3a1 1 0 001-1zM10 5a1 1 0 100 2 1 1 0 000-2z"
-      ></path>
-    </svg>
-    <span class="text-sm mr-4">{{ formatDate(page.update) }}</span>
-
-    <div class="flex flex-wrap" v-if="page.frontmatter.tags">
+  <div
+    class="page-meta flex items-center"
+    :class="{
+      'flex-col': layout !== 'home',
+    }"
+  >
+    <div class="dark:text-dark-text/[.86]">
+      <div class="inline-block text-sm mr-4">
+        🕔 {{ formatDate(page.date) }}
+      </div>
+      <div
+        v-if="layout !== 'home' && page.date != page.update"
+        class="inline-block text-sm mr-4"
+      >
+        {{ formatDate(page.update) }}
+      </div>
+      <div class="inline-block" v-if="showEditLink">
+        <span class="editLink"></span>
+        <a :href="editLink()" target="_blank">
+          {{ data.theme.value.editLink?.text }}
+        </a>
+      </div>
+    </div>
+    <div
+      class="meta-tag-list flex flex-wrap"
+      :class="{
+        'mt-3': layout !== 'home',
+        'basis-3/5': layout === 'home',
+        'flex-grow': layout === 'home',
+      }"
+      v-if="page.frontmatter.tags"
+    >
       <div
         v-for="tag in page.frontmatter.tags"
         class="px-2 py-1 bg-pink1 dark:bg-slate-300/20 rounded-lg text-xs mr-2"
       >
         <a :href="tagsUrl(layout, tag)"> {{ tag }} </a>
       </div>
-    </div>
-    <div v-if="showEditLink">
-      <span class="editLink"></span>
-      <a :href="editLink()" target="_blank">
-        {{ data.theme.value.editLink?.text }}
-      </a>
     </div>
   </div>
 </template>
