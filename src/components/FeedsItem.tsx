ae33d7007d73200c700869a35b1f3f3b2aadde98
fzdwx <likelovec@gmail.com>
2023-04-13 19:32:03 +0800

    Update css

--- src/components/FeedsItem.tsx
+++ src/components/FeedsItem.tsx
@@ -2,12 +2,21 @@ import { FeedItem } from "@/types";
 
 export default ({ item }: { item: FeedItem }) => {
   return (
-    <a target="_blank" class="links-feed-item" href={item.url} noIcon={true}>
-      <div class="flex flex-row links-feed-div  xl:w-[700px] 2xl:w-[800px]">
-        <header class=" basis-1/4 ">{item.name}</header>
-        <div class=" hover:text-dark-red basis-1/2">{item.title}</div>
-        <footer class="basis-1/4">{formatDate(item.time)}</footer>
-      </div>
-    </a>
+    <div class="flex flex-row links-feed-div  md:w-[700px] xl:w-[800px] 2xl:w-[800px]">
+      <header class=" mr-5 basis-1/4 text-ellipsis whitespace-nowrap  overflow-hidden    w-full">
+        {item.name}
+      </header>
+      <section class=" hover:text-dark-red  basis-1/2 whitespace-nowrap  overflow-hidden    w-full">
+        <a
+          target="_blank"
+          class="links-feed-item"
+          href={item.url}
+          noIcon={true}
+        >
+          {item.title}
+        </a>
+      </section>
+      <footer class="basis-1/4 text-ellipsis">{formatDate(item.time)}</footer>
+    </div>
   );
 };
