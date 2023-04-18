684e962b91b5fab30985efea82b885b4e5f1f5d7
fzdwx <likelovec@gmail.com>
2023-04-18 14:21:22 +0800

    feat: Feeds

--- src/components/FeedsItem.tsx
+++ src/components/FeedsItem.tsx
@@ -2,11 +2,11 @@ import { FeedItem } from "@/types";
 
 export default ({ item }: { item: FeedItem }) => {
   return (
-    <div class="flex flex-row links-feed-div  md:w-[700px] xl:w-[800px] 2xl:w-[800px]">
-      <header class=" mr-5 basis-1/4 text-ellipsis whitespace-nowrap  overflow-hidden    w-full">
-        {item.name}
+    <div class=" w-[95%] min-w-[400px] md:w-[700px] xl:w-[800px] 2xl:w-[800px] flex flex-row links-feed-div">
+      <header class="basis-1/6 opacity-50">
+        {formatDate(item.time, "M/DD")}
       </header>
-      <section class=" hover:text-dark-red  basis-1/2 whitespace-nowrap  overflow-hidden    w-full">
+      <section class=" hover:text-dark-red  basis-2/3 whitespace-nowrap overflow-hidden w-full">
         <a
           target="_blank"
           class="links-feed-item"
@@ -16,7 +16,9 @@ export default ({ item }: { item: FeedItem }) => {
           {item.title}
         </a>
       </section>
-      <footer class="basis-1/4 text-ellipsis">{formatDate(item.time)}</footer>
+      <footer class=" invisible sm:visible mr-5 basis-1/6 opacity-50 text-ellipsis whitespace-nowrap overflow-hidden w-full">
+        {item.name}
+      </footer>
     </div>
   );
 };
